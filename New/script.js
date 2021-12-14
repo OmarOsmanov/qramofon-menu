25406cdd47b4e1cbb218a074c911e0cc7f04e2c2
Osmanov <tu201906019@aztu.local>
2021-12-14 13:51:12 +0400

    first commit

---
+++ New/script.js
@@ -0,0 +1,9 @@
+document.readyState
+$(".accordion_tab").click(function(){
+    $(".accordion_tab").each(function(){
+      $(this).parent().removeClass("active");
+      $(this).removeClass("active");
+    });
+    $(this).parent().addClass("active");
+    $(this).addClass("active");
+});
